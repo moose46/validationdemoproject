f9047f9198e9242cd23ddb8fa7ce67950f527bf9
me <me@robertcurtiss.com>
2024-04-26 08:57:37 -0400

    22-80 Implementing a Single Clean ...

--- validationdemoapp/forms.py
+++ validationdemoapp/forms.py
@@ -38,9 +38,9 @@ class UserRegistrationForm(forms.ModelForm):
 
     # Field level validations
     def clean_phone_number(self):
-        iphonenumber = self.cleaned_data.get("phone_number")
-        if iphonenumber:
-            pattern = re.compile(f"(0|91)?[6-9][0-9]{9}")
+        if iphonenumber := self.cleaned_data.get("phone_number"):
+            # pattern = re.compile(f"(0|91)?[6-9][0-9]{9}")
+            pattern = re.compile("^(([0-9]{3}) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$")
             if not re.fullmatch(pattern, iphonenumber):
                 raise forms.ValidationError(
                     "Invalid Phone Number! Example: +911234567891"
@@ -51,13 +51,20 @@ class UserRegistrationForm(forms.ModelForm):
         cleaned_data = super().clean()
         ipassword = cleaned_data.get("password")
         iconfirm_password = cleaned_data.get("confirm_password")
-        if ipassword and iconfirm_password:
-            if ipassword != iconfirm_password:
-                raise forms.ValidationError("Passwords do not match.")
+        if ipassword and iconfirm_password and ipassword != iconfirm_password:
+            raise forms.ValidationError("Passwords do not match.")
+
         iusername = cleaned_data.get("username")
-        if ipassword and iusername:
-            if ipassword == iusername:
-                raise forms.ValidationError(
-                    "User Name and password should not be the same..."
-                )
+        if ipassword and iusername and ipassword == iusername:
+            raise forms.ValidationError(
+                "User Name and password should not be the same..."
+            )
+
+        country = cleaned_data.get("country")
+        if country == "select":
+            raise forms.ValidationError("Please Choose a Country.")
+
+        if not cleaned_data.get("terms_conditions"):
+            raise forms.ValidationError("Please agree to terms and condidtions")
+
         return cleaned_data
